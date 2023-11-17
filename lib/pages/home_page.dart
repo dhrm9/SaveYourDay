8ffa0ca0f8746e2a31b4684079e8317a2575b2ad
dhrm9 <dharamendrasingh11@gmail.com>
2023-11-18 00:11:40 +0530

    added error handling in taskname and description

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,24 +44,50 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-  
-
   //check box was tapped
-  void checkBoxChanged(bool? value,int index) {
+  void checkBoxChanged(bool? value, int index) {
     setState(() {
       db.toDoList[index].isCompleted = !db.toDoList[index].isCompleted;
     });
     db.updateDataBase();
   }
 
-  //save new task
   void saveNewTask() {
+    // Check if the task name, description, and image path are not empty
+    if (_controller.text.isEmpty || _desciptionController.text.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content:
+                const Text('Task name, description, and image are required.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return; // Exit the method if there is an error
+    }
+
     setState(() {
+      int newId = generateRandomNumber(1000, 9999);
 
-      int newId = generateRandomNumber(1000 , 9999);
+      Task newTask = Task(
+        accessType: "Private",
+        taskDescription: _desciptionController.text,
+        taskId: newId,
+        taskName: _controller.text,
+        taskTag: taskTag[0],
+        isCompleted: false,
+      );
 
-      Task newTask = Task(accessType: "Private", taskDescription: _desciptionController.text, taskId: newId, taskName: _controller.text, taskTag:  taskTag[0] , isCompleted: false);
-      
       db.toDoList.add(newTask);
       _controller.clear();
       _desciptionController.clear();
@@ -98,11 +124,14 @@ class _HomePageState extends State<HomePage> {
     FirebaseAuth.instance.signOut();
   }
 
-  void edit(List t){
+  void edit(List t) {
+    int index = t[0];
+    Task updatedTask = t[1];
+
     setState(() {
-       db.toDoList[t[0]] = t[1];
-   
+      db.toDoList[index] = updatedTask;
     });
+
     Navigator.of(context).pop();
     db.updateDataBase();
   }
@@ -112,15 +141,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
-        actions: [IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))],
-        title: Text('TO DO'),
+        actions: [
+          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
+        ],
+        title: const Text('TO DO'),
         elevation: 0,
       ),
 
       //button to add the tasks
       floatingActionButton: FloatingActionButton(
         onPressed: createNewTask,
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
 
       //build a dynamic list so that we can add later by + button
